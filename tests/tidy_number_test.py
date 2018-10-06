96d835adbb4620678fb4f126e3433d5c09cd16da
r2dong <r2dong@yahoo.com>
2018-10-06 14:44:28 -0500

    tidy_number working for large inputs

--- tests/tidy_number_test.py
+++ tests/tidy_number_test.py
@@ -13,5 +13,8 @@ class TestTidyNumbers(unittest.TestCase):
     def test_small_2(self):
         self.assertEqual('7', tidy_number('7'))
 
-    def _test_small_3(self):
-        pass
+    def test_large_0(self):
+        self.assertEqual('99999999999999999', tidy_number('111111111111111110'))
+
+    def test_large_1(self):
+        self.assertEqual('119999999999999999', tidy_number('122222211635179876'))
